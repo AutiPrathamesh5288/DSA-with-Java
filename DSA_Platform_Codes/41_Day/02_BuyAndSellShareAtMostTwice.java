0a7faa74a8e5d0e9a7df8f27654b6ef72a74ea4a
AutiPrathamesh5288 <prathameshauti004@gmail.com>
2024-02-26 22:40:30 +0530

    Day 41 Completed

---
+++ DSA_Platform_Codes/41_Day/02_BuyAndSellShareAtMostTwice.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static int maxProfit(int n, int[] price) {
+        if (n <= 1) {
+            return 0;
+        }
+
+        int[] profit1 = new int[n];
+        int[] profit2 = new int[n];
+
+        int minPrice = price[0];
+        for (int i = 1; i < n; i++) {
+            minPrice = Math.min(minPrice, price[i]);
+            profit1[i] = Math.max(profit1[i - 1], price[i] - minPrice);
+        }
+
+        int maxPrice = price[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            maxPrice = Math.max(maxPrice, price[i]);
+            profit2[i] = Math.max(profit2[i + 1], maxPrice - price[i]);
+        }
+
+        int maxProfit = 0;
+        for (int i = 0; i < n; i++) {
+            maxProfit = Math.max(maxProfit, profit1[i] + (i + 1 < n ? profit2[i + 1] : 0));
+        }
+
+        return maxProfit;
+    }
+}
